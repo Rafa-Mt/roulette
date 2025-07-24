15458b67d60328fcb7d33208cff8a81b35b5bee0
MarlonUrda <116409683+MarlonUrda@users.noreply.github.com>
2025-07-24 01:53:14 -0400

    odio todo y mis quemadas me arden

--- auth_microservice/src/service/redis.py
+++ auth_microservice/src/service/redis.py
@@ -1,14 +1,14 @@
 from redis import Redis
 
 session_storage = Redis(
-  host="redis",
+  host="localhost",
   port=6379,
   db=0,
   decode_responses=True
 )
 
 cache_storage = Redis(
-  host="redis",
+  host="localhost",
   port=6379,
   db=1,
   decode_responses=True
