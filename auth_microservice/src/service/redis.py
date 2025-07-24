ad3f584531cbaad0d5220fa981d95028b4bf791b
MarlonUrda <116409683+MarlonUrda@users.noreply.github.com>
2025-07-24 11:10:51 -0400

    no me gusta docker

--- auth_microservice/src/service/redis.py
+++ auth_microservice/src/service/redis.py
@@ -1,14 +1,14 @@
 from redis import Redis
 
 session_storage = Redis(
-  host="localhost",
+  host="redis",
   port=6379,
   db=0,
   decode_responses=True
 )
 
 cache_storage = Redis(
-  host="localhost",
+  host="redis",
   port=6379,
   db=1,
   decode_responses=True
