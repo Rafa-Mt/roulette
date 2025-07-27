012945b3ab13ee7eb62dba7b22d21d5017b4234e
Tomas Santana <tomas.30604530@uru.edu>
2025-07-27 17:36:44 -0400

    fix: data model

--- backend/data_model.sql
+++ backend/data_model.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(200) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    balance: NUMERIC(10, 2) DEFAULT 0.00
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    balance NUMERIC(10, 2) DEFAULT 0.00
 );
 
 -- Table to store user balances
